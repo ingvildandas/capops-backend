cb5391acdb40c123b31f06899a5e178fd73ebfe8
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-03-06 10:15:39 +0100

    CREATE: Added a collection of Track structs to facilitate easier serialization

---
+++ include/Structs/TrackStructs.hpp
@@ -0,0 +1,31 @@
+#include <string>
+
+struct TrackGlobalPosition
+{
+    const std::string frame;
+    const double latitudeDegrees;
+    const double longitudeDegrees;
+    const double altitudeMeters;
+};
+
+struct TrackLocalPosition
+{
+    const std::string frame;
+    const double xPosition;
+    const double yPosition;
+    const double zPosition;
+};
+
+struct TrackGlobalVelocity
+{
+    const double totalVelocityMetersPerSecond;
+    const double groundVelocityMetersPerSecond;
+    const double verticalVelocityMetersPerSecond;
+};
+
+struct TrackLocalVelocity
+{
+    const double xVelocityMetersPerSecond;
+    const double yVelocityMetersPerSecond;
+    const double zVelocityMetersPerSecond;
+};
