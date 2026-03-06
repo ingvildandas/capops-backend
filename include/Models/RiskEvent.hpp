4c891616bf90a2f1c7a47986e6e93c1b994c80d6
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-03-06 10:34:47 +0100

    UPDATE: Definition for RiskEvent

--- include/Models/RiskEvent.hpp
+++ include/Models/RiskEvent.hpp
@@ -1,8 +1,26 @@
 #pragma once
 
+#include <string>
+
 class RiskEvent
 {
 public:
-    RiskEvent();
+    RiskEvent(
+        const int riskEventId,
+        const int riskSeverity,
+        const std::string& message,
+        const bool acknowledged
+    );
+
+    const int getRiskEventId();
+    const int getRiskSeverity();
+    const std::string& getMessage();
+    const bool getAcknowledged();
+
+private:
+    int _riskEventId;
+    int _riskSeverity;
+    std::string _message;
+    bool acknowledged;
 };
 
