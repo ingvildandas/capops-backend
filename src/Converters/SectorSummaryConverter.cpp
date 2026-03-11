ffc61110e2bed3449ccb290e130624d62a88c7af
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-03-11 09:29:50 +0100

    CREATE: Implementation for SectorSummaryConverter

---
+++ src/Converters/SectorSummaryConverter.cpp
@@ -0,0 +1,77 @@
+#include <string>
+#include <vector>
+
+#include <QJsonArray>
+#include <QJsonObject>
+
+#include "Converters/SectorSummaryConverter.hpp"
+#include "Models/SectorSummary.hpp"
+
+
+SectorSummary SectorSummaryConverter::fromJson(const QJsonObject& json)
+{
+    int sectorId = json["sectorId"].toInt();
+    int row = json["row"].toInt();
+    int colunmn = json["column"].toInt();
+    int weatherSeverity = json["weatherSeverity"].toInt();
+    double weatherFactor = json["double"].toDouble();
+    int localAircraftCount = json["localAircraftCount"].toInt();
+    int localAircraftBaseCapacity = json["localAircraftBaseCapacity"].toInt();
+    int localAircraftEffectiveCapacity = 
+        json["localAircraftEffectiveCapacity"].toInt();
+    
+    return SectorSummary
+    (
+        sectorId,
+        row,
+        colunmn,
+        weatherSeverity,
+        weatherFactor,
+        localAircraftCount,
+        localAircraftBaseCapacity,
+        localAircraftEffectiveCapacity
+    );
+}
+
+std::vector<SectorSummary> SectorSummaryConverter::fromJson(const QJsonArray& jsonArray)
+{
+    std::vector<SectorSummary> sectorSummaries;
+    sectorSummaries.reserve(jsonArray.size());
+
+    for (const auto& value : jsonArray)
+    {
+        QJsonObject obj = value.toObject();
+        sectorSummaries.push_back(fromJson(obj));
+    }
+
+    return sectorSummaries;
+}
+
+QJsonObject SectorSummaryConverter::toJson(const SectorSummary& sectorSummary)
+{
+    return 
+    {
+        { "sectorId", sectorSummary.getSectorId() },
+        { "row", sectorSummary.getRow() },
+        { "colunmn", sectorSummary.getColunmn() },
+        { "weatherSeverity", sectorSummary.getWeatherSeverity() },
+        { "weatherFactor", sectorSummary.getWeatherFactor() },
+        { "localAircraftCount", sectorSummary.getLocalAircraftCount() },
+        { "localAircraftBaseCapacity", sectorSummary.getLocalAircraftBaseCapacity() },
+        { 
+            "localAircraftEffectiveCapacity", 
+            sectorSummary.getLocalAircraftEffectiveCapacity() 
+        }
+    };
+}
+
+QJsonArray SectorSummaryConverter::toJson(const std::vector<SectorSummary>& sectorSummaries)
+{
+    QJsonArray sectorSummaryArray;
+    for (SectorSummary sectorSummary : sectorSummaries)
+    {
+        sectorSummaryArray.append(toJson(sectorSummary));
+    }
+
+    return sectorSummaryArray;
+}
