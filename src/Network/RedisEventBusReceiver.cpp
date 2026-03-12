5ee3501c204d072a0d4b1a87f830702838397529
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-03-12 10:40:56 +0100

    CREATE: Implementation of RedisEventBusReceiver

---
+++ src/Network/RedisEventBusReceiver.cpp
@@ -0,0 +1,112 @@
+#include <atomic>
+#include <memory>
+#include <thread>
+
+#include <QObject>
+#include <QString>
+
+#include <sw/redis++/redis++.h>
+
+#include "Network/RedisEventBusReceiver.hpp"
+#include "Converters/FlightDataDtoConverter.hpp"
+#include "Dtos/FlightDataDto.hpp"
+#include "Managers/WebSocketSessionManager.hpp"
+#include "Services/RiskEventService.hpp"
+#include "Services/SectorSummaryService.hpp"
+#include "Services/TrackService.hpp"
+
+RedisEventBusReceiver::RedisEventBusReceiver
+(
+    const QString& redisUri,
+    WebSocketSessionManager& sessionManager,
+    RiskEventService& riskEventService,
+    SectorSummaryService& sectorSummaryService,
+    TrackService& trackService,
+    QObject* parent
+)
+    :
+    _redisUri(redisUri),
+    _sessionManager(sessionManager),
+    _sectorSummaryService(sectorSummaryService),
+    _trackService(trackService),
+    _riskEventService(riskEventService),
+    QObject(parent)
+{}
+
+RedisEventBusReceiver::~RedisEventBusReceiver()
+{
+    stop();
+}
+
+void RedisEventBusReceiver::start()
+{
+    if (_running) return;
+
+    _redis = std::make_unique<sw::redis::Redis>(_redisUri);
+    _running = true;
+
+    _subscriberThread = std::thread([this]()
+    {
+        runSubscriber();
+    });
+}
+
+void RedisEventBusReceiver::stop()
+{
+    _running = false;
+
+    if (_subscriberThread.joinable())
+    {
+        _subscriberThread.join();
+    }
+}
+
+void RedisEventBusReceiver::runSubscriber()
+{
+    auto sub = _redis->subscriber();
+
+    sub.on_message([this](std::string channel, std::string msg)
+    {
+        handleMessage(channel, msg);
+    });
+
+    sub.subscribe("flightdata");
+
+    while (_running)
+    {
+        try
+        {
+            sub.consume();
+        }
+        catch (const sw::redis::Error &e)
+        {
+            qWarning() << "Redis error:" << e.what();
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    }
+}
+
+void RedisEventBusReceiver::handleMessage(const std::string& channel, const std::string& payload)
+{
+    try
+    {
+        auto dto = FlightDataDtoConverter::fromProtobuf(QString::fromStdString(payload));
+
+        if (!dto.getTracks().empty())
+        {
+            _trackService.updateState(dto.getTracks());
+        }
+
+        if (!dto.getSectorSummaries().empty())
+            _sectorSummaryService.updateState(dto.getSectorSummaries());
+
+        if (!dto.getSectorSummaries().empty())
+            _riskEventService.updateState(dto.getRiskEvents());
+
+        _sessionManager.broadcast(dto);
+    }
+    catch (const std::exception& e)
+    {
+        qWarning() << "Event bus error: " << e.what();
+    }
+}
