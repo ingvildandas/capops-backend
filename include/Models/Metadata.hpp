74944bf8d3de9bb47ce1b44b9550c6e5e01434bc
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-03-13 16:10:51 +0100

    CREATE: Implementation for Metadata entity

--- include/Models/Metadata.hpp
+++ include/Models/Metadata.hpp
@@ -5,12 +5,17 @@
 class Metadata
 {
 public:
+    static void initializeVersionControl(const int version);
+    static void cleanupVersionControl();
+    static bool getIsInitialized();
+
     Metadata(const QDateTime& timestamp);
 
     const QDateTime& getTimestamp() const;
 public:
-    static int version;
 
 private:
+    static int _version;
+    static bool _isInitialized;
     QDateTime _timestamp;
 };
