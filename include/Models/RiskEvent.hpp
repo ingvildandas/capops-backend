e5459c655057a29ebd21260ec5f7ff09a9cd484e
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-03-13 08:52:47 +0100

    UPDATE: RiskEvent entity now use QString instead of std string

--- include/Models/RiskEvent.hpp
+++ include/Models/RiskEvent.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <string>
+#include <QString>
 
 class RiskEvent
 {
@@ -8,19 +8,19 @@ public:
     RiskEvent(
         const int riskEventId,
         const int riskSeverity,
-        const std::string& message,
+        const QString& message,
         const bool acknowledged
     );
 
     const int getRiskEventId() const;
     const int getRiskSeverity() const;
-    const std::string& getMessage() const;
+    const QString& getMessage() const;
     const bool getAcknowledged() const;
 
 private:
     int _riskEventId;
     int _riskSeverity;
-    std::string _message;
+    QString _message;
     bool _acknowledged;
 };
 
