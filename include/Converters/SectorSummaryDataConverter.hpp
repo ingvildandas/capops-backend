ffe7eff8f970e4aa09ed5a8391c5f80102a6855e
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-03-16 14:18:32 +0100

    CREATE: Implementation for SectorSummaryDataConverter

---
+++ include/Converters/SectorSummaryDataConverter.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vector>
+
+#include "Proto/FlightData.hpp"
+
+class SectorSummary;
+class SectorSummaryDataProto;
+class QJsonObject;
+
+class SectorSummaryDataConverter
+{
+public:
+    static SectorSummary fromJson(const QJsonObject& json);
+    static QJsonObject toJson(const SectorSummaryData& sectorSummary);
+    static SectorSummary fromProto
+    (
+        const SectorSummaryDataProto& protoSectorSummary
+    );
+
+    SectorSummaryDataConverter() = delete;
+};
---
+++ src/Converters/SectorSummaryDataConverter.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+
+#include <QJsonObject>
+#include <QString>
+
+#include "Converters/SectorSummaryDataConverter.hpp"
+#include "Converters/SectorSummaryConverter.hpp"
+#include "Models/SectorSummary.hpp"
+#include "Models/SectorSummaryData.hpp"
+#include "Proto/FlightData.hpp"
+
+SectorSummaryData SectorSummaryDataConverter::fromJson(const QJsonObject& json)
+{
+    int totalAircraftCount = json["totalAircraftCount"].toInt();
+    std::vector<SectorSummary> sectorSummaries = 
+        SectorSummaryConverter::fromJson(json["sectorSummaries"].toArray());
+
+    return SectorSummaryData(totalAircraftCount, sectorSummaries);
+}
+
+QJsonObject SectorSummaryDataConverter::toJson
+(
+    const SectorSummaryData& sectorSummaryData
+)
+{
+    return QJsonObject
+    {
+        { "totalAircraftCount", sectorSummaryData.getTotalAircraftCount() },
+        { 
+            "sectorSummaries", 
+            SectorSummaryConverter::toJson(sectorSummaryData.getSectorSummaries()) 
+        }
+    };
+}
+
+SectorSummaryData SectorSummaryDataConverter::fromProto
+(
+    const SectorSummaryDataProto& protoSectorSummary
+)
+{
+    int totalAircraftCount = protoSectorSummary.totalaircraftcount();
+    std::vector<SectorSummary> sectorSummaries = 
+        SectorSummaryConverter::fromProto(protoSectorSummary.sectorsummaries());
+
+    return SectorSummaryData(totalAircraftCount, sectorSummaries);
+}
