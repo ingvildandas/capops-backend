665b4c6019b15fc0a3a24d41cc81b4a3120dcfdd
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-03-16 09:26:55 +0100

    UPDATE: Modified RiskEvent

--- src/Models/RiskEvent.cpp
+++ src/Models/RiskEvent.cpp
@@ -5,15 +5,21 @@
 RiskEvent::RiskEvent
 (
     const int riskEventId,
-    const int riskSeverity,
-    const QString& message,
-    const bool acknowledged
+    const int sectorId,
+    const bool acknowledged,
+    const QString& riskSeverity,
+    const QDateTime& createdTimestamp,
+    const QDateTime& acknowledgedTimestamp,
+    const QString& message
 )
     : 
     _riskEventId(riskEventId), 
+    _sectorId(sectorId),
+    _acknowledged(acknowledged),
     _riskSeverity(riskSeverity),
-    _message(message),
-    _acknowledged(acknowledged)
+    _createdTimestamp(createdTimestamp),
+    _acknowledgedTimestamp(acknowledgedTimestamp),
+    _message(message)
 {}
 
 const int RiskEvent::getRiskEventId() const 
@@ -21,17 +27,32 @@ const int RiskEvent::getRiskEventId() const
     return _riskEventId;
 }
 
-const int RiskEvent::getRiskSeverity() const
+const int RiskEvent::getSectorId() const
 {
-    return _riskSeverity;
-}
-
-const QString& RiskEvent::getMessage() const
-{
-    return _message;
+    return _sectorId;
 }
 
 const bool RiskEvent::getAcknowledged() const
 {
     return _acknowledged;
+}
+
+const QString& RiskEvent::getRiskSeverity() const
+{
+    return _riskSeverity;
+}
+
+const QDateTime& RiskEvent::getCreatedTimestamp() const
+{
+    return _createdTimestamp;
+}
+
+const QDateTime& RiskEvent::getAcknowledgedTimestamp() const
+{
+    return _acknowledgedTimestamp;
+}
+
+const QString& RiskEvent::getMessage() const
+{
+    return _message;
 }
