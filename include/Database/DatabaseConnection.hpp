cc06b0cdabfc2739a18aba66bf63bfd113aa11b2
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-03-17 10:16:36 +0100

    UPDATE: DatabaseConnection now using prepared statements

--- include/Database/DatabaseConnection.hpp
+++ include/Database/DatabaseConnection.hpp
@@ -18,8 +18,14 @@ public:
     DatabaseConnection& operator=(DatabaseConnection&&) noexcept;
 
     sqlite3* getHandle();
-    void execute(const std::string& query);
-    ResultSet executeResult(const std::string& query);
+    sqlite3_stmt* prepareStatement(const std::string& query);
+
+    void bindText(sqlite3_stmt* stmt, int index, const std::string& value);
+    void bindInt(sqlite3_stmt* stmt, int index, int value);
+    void bindBool(sqlite3_stmt* stmt, int index, bool value);
+    
+    void finalizeStatement(sqlite3_stmt* stmt);
+    ResultSet finalizeStatementWithResult(sqlite3_stmt* stmt);
 private:
     sqlite3* _handle{nullptr};
 };
