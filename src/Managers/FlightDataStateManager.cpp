720fdf7f7463d641dd68c37f68fb2a75613955cc
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-03-17 13:52:21 +0100

    FlightDataState now stores unique pointers, due to domains not having default constructors by intent

--- src/Managers/FlightDataStateManager.cpp
+++ src/Managers/FlightDataStateManager.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 
 #include "Managers/FlightDataStateManager.hpp"
+#include "Exceptions/EntityModelException.hpp"
 #include "Models/FlightData.hpp"
 #include "Models/Metadata.hpp"
 #include "Models/RiskEventData.hpp"
@@ -15,33 +16,44 @@ FlightDataStateManager::FlightDataStateManager()
 
 FlightData FlightDataStateManager::getState()
 {
+    if 
+    (
+        !_state->metadata || 
+        !_state->riskEventData || 
+        !_state->sectorSummaryData || 
+        !_state->trackData
+    ) 
+    {
+        throw EntityModelException("FlightDataState is not fully initialized.");
+    }
+
     return FlightData
     (
-        _state->metadata, 
-        _state->riskEventData, 
-        _state->sectorSummaryData, 
-        _state->trackData
+        *_state->metadata, 
+        *_state->riskEventData, 
+        *_state->sectorSummaryData, 
+        *_state->trackData
     );
 }
 
 void FlightDataStateManager::setMetadata(const Metadata& metadata)
 {
-    _state->metadata = metadata;
+    _state->metadata = std::make_unique<Metadata>(metadata);
 }
 
 void FlightDataStateManager::setRiskEventData(const RiskEventData& riskEventData)
 {
-    _state->riskEventData = riskEventData;
+    _state->riskEventData = std::make_unique<RiskEventData>(riskEventData);
 }
 
 void FlightDataStateManager::setSectorSummaryData(const SectorSummaryData& sectorSummaryData)
 {
-    _state->sectorSummaryData = sectorSummaryData;
+    _state->sectorSummaryData = std::make_unique<SectorSummaryData>(sectorSummaryData);
 }
 
 void FlightDataStateManager::setTrackData(const TrackData& trackData)
 {
-    _state->trackData = trackData;
+    _state->trackData = std::make_unique<TrackData>(trackData);
 }
 
 void FlightDataStateManager::resetState()
