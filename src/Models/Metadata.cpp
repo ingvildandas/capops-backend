b4cdcfefafcfc0d94b350e76f170dd31f81db31e
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-03-17 15:58:37 +0100

    UPDATE: Version control removed from Metadata entity

--- src/Models/Metadata.cpp
+++ src/Models/Metadata.cpp
@@ -4,48 +4,9 @@
 
 #include "Models/Metadata.hpp"
 
-void Metadata::initializeVersionControl(const int version)
-{
-    if (version < 1) 
-    {
-        throw new std::runtime_error("Cannot set non-positive version");
-    }
-    _version = version;
-    _isInitialized = true;
-}
-void Metadata::cleanupVersionControl()
-{
-    _version = 0;
-    _isInitialized = false;
-}
-
-bool Metadata::getIsInitialized()
-{
-    if (!_isInitialized || _version < 1)
-    {
-        return false;
-    }
-
-    return true;
-}
-
-int Metadata::getVersion()
-{
-    if (!getIsInitialized())
-    {
-        throw new std::runtime_error("Version control not initialized");
-    }
-
-    return _version;
-}
-
-Metadata::Metadata(const QDateTime& timestamp) : _timestamp(timestamp)
-{
-    if (!getIsInitialized())
-    {
-        throw new std::runtime_error("Version control not initialized");
-    }
-}
+Metadata::Metadata(const QDateTime& timestamp)
+    : _timestamp(timestamp)
+{}
 
 const QDateTime& Metadata::getTimestamp() const
 {
