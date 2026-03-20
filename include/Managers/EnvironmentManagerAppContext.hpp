1e614b0a72baee65c988d94d932f8ce53b5dbe20
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-03-20 14:07:18 +0100

    EnvironmentManagerAppContext should now read env vars from app runtime context

--- include/Managers/EnvironmentManagerAppContext.hpp
+++ include/Managers/EnvironmentManagerAppContext.hpp
@@ -8,7 +8,9 @@
 class EnvironmentManagerAppContext : public IEnvironmentManager
 {
 public:
-    EnvironmentManagerAppContext() = default;
+    EnvironmentManagerAppContext();
+
+    void loadEnvironmentVariables() const;
 
     QString getDatabaseFilePath() const override;
     quint16 getHttpPort() const override;
@@ -16,4 +18,12 @@ public:
     QString getRedisUri() const override;
     int getProtoBufContractVersion() const override;
     int getRestJsonContractVersion() const override;
+
+private:
+    mutable QString _databaseFilePath;
+    mutable quint16 _httpPort;
+    mutable quint16 _webSocketPort;
+    mutable QString _redisUri;
+    mutable int _protoBufContractVersion;
+    mutable int _restJsonContractVersion;
 };
