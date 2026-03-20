9e8088c9ba776a726441affa92cc2dd0b811e150
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-03-20 13:56:54 +0100

    CREATE: Implementation for EnvironmentManagerTest

---
+++ include/Managers/EnvironmentManagerTest.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <QString>
+#include <QtGlobal>
+
+#include "IEnvironmentManager.hpp"
+
+class EnvironmentManagerTest : public IEnvironmentManager
+{
+public:
+    EnvironmentManagerTest() = default;
+
+    QString getDatabaseFilePath() const override;
+    quint16 getHttpPort() const override;
+    quint16 getWebSocketPort() const override;
+    QString getRedisUri() const override;
+    int getProtoBufContractVersion() const override;
+    int getRestJsonContractVersion() const override;
+};
