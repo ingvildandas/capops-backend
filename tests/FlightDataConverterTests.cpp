ef547a88dc4357adef8c364e97187ad20d75f763
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-03-24 16:08:30 +0100

    UPDATE: Added test

--- tests/FlightDataConverterTests.cpp
+++ tests/FlightDataConverterTests.cpp
@@ -288,6 +288,8 @@ TEST_CASE("Serialize valid FlightData to JSON", "[FlightDataConverter]")
     REQUIRE(mergedRiskEventJson["sectorId"].toInt() == 2);
     REQUIRE(mergedRiskEventJson["summaryMessage"].toString() == "Risk severity in sector 2 changed from AT_RISK to CONGESTED");
     REQUIRE(mergedRiskEventJson["lastMessage"].toString() == "Test risk event 3");
+    qDebug() << mergedRiskEventJson["checksum"].toString();
+    REQUIRE(mergedRiskEventJson["checksum"].toString() == "2/3");
 
     const auto sectorSummaryDataJson = json["sectorSummaryData"].toObject();
     REQUIRE(sectorSummaryDataJson["rowsCount"].toInt() == 2);
