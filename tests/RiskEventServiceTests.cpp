5d193e4eb9e262690477c5b8460778353df63d71
Jacob Gomez Hansen <jacobg431@gmail.com>
2026-03-25 09:58:22 +0100

    CREATE: RiskEventServiceTests

---
+++ tests/RiskEventServiceTests.cpp
@@ -0,0 +1,121 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <vector>
+
+#include <QDateTime>
+
+#include "Managers/FlightDataStateManager.hpp"
+#include "Models/FlightData.hpp"
+#include "Models/RiskEvent.hpp"
+#include "Models/RiskEventData.hpp"
+#include "Repositories/IRiskEventRepository.hpp"
+#include "Services/RiskEventService.hpp"
+
+class MockRiskEventRepository : public IRiskEventRepository {
+public:
+    RiskEvent selectById(int) override { return {}; }
+    std::vector<RiskEvent> selectMultipleByCount(int) override { return {}; }
+    std::vector<RiskEvent> selectMultipleByAcknowledged(int,bool) override { return {}; }
+    std::vector<RiskEvent> selectMultipleByTimestamps(int,const QDateTime&,const QDateTime&) override { return {}; }
+    std::vector<RiskEvent> selectMultipleByParameters(int,bool,const QDateTime&,const QDateTime&) override { return {}; }
+    void insert(const RiskEvent&) override {}
+    void insertMultiple(const std::vector<RiskEvent>&) override {}
+    void updateAcknowledged(const int riskEventId, const bool acknowledged) override {}
+    void updateMultipleAcknowledged(const std::vector<int>&) override {}
+    void deleteById(int) override {}
+};
+
+TEST_CASE("Risk events are merged correctly", "[RiskEventService]")
+{
+    // Arrange 
+
+    MockRiskEventRepository repo;
+    RiskEventService svc(repo);
+    FlightDataStateManager stateManager;
+
+    RiskEvent riskEvent1
+    (
+        1, 
+        0, 
+        false, 
+        "NORMAL", 
+        QDateTime::fromString("2024-06-01T12:00:00.000Z"),
+        QDateTime(),
+        "Test risk event 1"
+    );
+    
+    RiskEvent riskEvent2
+    (
+        2, 
+        0, 
+        false, 
+        "CONGESTED", 
+        QDateTime::fromString("2024-06-01T12:10:00.000Z"),
+        QDateTime(),
+        "Test risk event 2"
+    );
+    
+    RiskEvent riskEvent3
+    (
+        3, 
+        0, 
+        false, 
+        "AT_RISK", 
+        QDateTime::fromString("2024-06-01T12:20:00.000Z"),
+        QDateTime(),
+        "Test risk event 3"
+    );
+
+    std::vector<RiskEvent> existingRiskEvents;
+    existingRiskEvents.push_back(riskEvent1);
+    existingRiskEvents.push_back(riskEvent2);
+    RiskEventData existingRiskEventData(2, existingRiskEvents);
+
+    std::vector<RiskEvent> incomingRiskEvents;
+    existingRiskEvents.push_back(riskEvent3);
+    RiskEventData incomingRiskEventData(1, incomingRiskEvents);
+
+    stateManager.setRiskEventData(existingRiskEventData);
+
+    // Act
+    svc.updateState(incomingRiskEventData, stateManager);
+
+    // Assert
+    const auto updatedMergedRiskEvents = 
+        stateManager.getState().getRiskEventData().getMergedRiskEvents();
+    
+    REQUIRE(updatedMergedRiskEvents.size() == 1);
+
+    const auto& mergedEvents = updatedMergedRiskEvents[0].getRiskEvents();
+    REQUIRE(mergedEvents.size() == 3);
+
+    REQUIRE
+    (
+        std::find_if
+        (
+            mergedEvents.begin(), 
+            mergedEvents.end(),
+            [&](const RiskEvent& r){ return r.getRiskEventId()==1; }
+        ) != mergedEvents.end()
+    );
+
+    REQUIRE
+    (
+        std::find_if
+        (
+            mergedEvents.begin(), 
+            mergedEvents.end(),
+            [&](const RiskEvent& r){ return r.getRiskEventId()==2; }
+        ) != mergedEvents.end()
+    );
+    
+    REQUIRE
+    (
+        std::find_if
+        (
+            mergedEvents.begin(), 
+            mergedEvents.end(),
+            [&](const RiskEvent& r){ return r.getRiskEventId()==3; }
+        ) != mergedEvents.end()
+    );
+}
